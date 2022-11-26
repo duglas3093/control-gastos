cd857d8117ddbcafd3b661908ccdc900592a96b0
douglas quisbert <duglas3093@gmail.com>
2022-11-25 21:14:14 -0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { ControlPresupuesto } from './ControlPresupuesto'
+import { NuevoPresupuesto } from './NuevoPresupuesto'
+
+export const Header = ({ 
+    presupuesto, 
+    setPresupuesto, 
+    isValidPresupuesto, 
+    setIsValidPresupuesto,
+    gastos,
+    setGastos
+    }) => {
+    return (
+        <header>
+            <h1>Planificador de gastos</h1>
+            { isValidPresupuesto ? (
+                <ControlPresupuesto 
+                    presupuesto={presupuesto} 
+                    gastos={gastos}
+                    setGastos={setGastos}
+                    setPresupuesto={setPresupuesto}
+                    setIsValidPresupuesto={setIsValidPresupuesto}
+                />
+            ):(
+                <NuevoPresupuesto
+                    presupuesto={presupuesto}
+                    setPresupuesto={setPresupuesto}
+                    setIsValidPresupuesto={setIsValidPresupuesto}
+                />
+            )}
+            
+        </header>
+    )
+}
